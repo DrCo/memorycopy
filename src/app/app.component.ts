de4df38a8334051b006e07a53020ab1c702d550c
DrCo <drcoepic@gmail.com>
2018-04-21 16:18:40 -0700

    four on Saturday

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RecordPage } from '../pages/record/record';
+import { SharePage } from '../pages/share/share';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +25,10 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Record', component: RecordPage },
+      { title: 'Share', component: SharePage },
+      { title: 'Login', component: LoginPage }
     ];
 
   }
